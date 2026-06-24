c07e2661b612281256d3f940ceb2f452f820b571
Rudransh Singh <rudranshsingh57112@gmail.com>
2026-06-24 17:26:07 +0530

    added frontend to CORS allowlist

--- backend/main.py
+++ backend/main.py
@@ -59,7 +59,7 @@ app.add_middleware(GZipMiddleware, minimum_size=500)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://pdf-buddy-red.vercel.app"],
     allow_credentials=False,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
